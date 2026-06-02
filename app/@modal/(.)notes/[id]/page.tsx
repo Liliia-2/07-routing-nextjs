1c628c07b66af9eee6205b53579e29907a87a038
Liliia <plv-86@ukr.net>
2026-06-02 16:06:01 +0300

    commit message

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,2 +1,18 @@
+'use client';
 
-import NotePreviewClient from "./NotePreview.client";
+import { useParams, useRouter } from 'next/navigation';
+import Modal from '@/components/Modal/Modal';
+import NotePreviewClient from './NotePreview.client';
+
+export default function ModalPage() {
+  const router = useRouter();
+  const params = useParams();
+
+  const id = params.id as string;
+
+  return (
+    <Modal onClose={() => router.back()}>
+      <NotePreviewClient id={id} />
+    </Modal>
+  );
+}
