56923b7bd6f1e350d01d48ab97e15741231fe01c
Liliia <plv-86@ukr.net>
2026-06-02 18:26:40 +0300

    commit update page

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,31 +1,28 @@
 import {
-    QueryClient,
-    dehydrate,
-    HydrationBoundary,
+  QueryClient,
+  dehydrate,
+  HydrationBoundary,
 } from '@tanstack/react-query';
+
 import { fetchNoteById } from '@/lib/api';
-// import { useParams, useRouter } from 'next/navigation';
-// import Modal from '@/components/Modal/Modal';
 import NotePreviewClient from './NotePreview.client';
 
-interface PageProps {
-    params: Promise<{
-        id: string;
-    }>;
-}
-export default async function Page({
-    params,
-}: PageProps) {
-    const { id } = await params;
-    const queryClient = new QueryClient();
-    await queryClient.prefetchQuery({
-        queryKey: ['note', id],
-        queryFn: () => fetchNoteById(id),
-    });
-    return (
-    <HydrationBoundary
-      state={dehydrate(queryClient)}
-    >
+export default async function ModalPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <NotePreviewClient id={id} />
     </HydrationBoundary>
   );
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,29 @@
+import {
+  QueryClient,
+  dehydrate,
+  HydrationBoundary,
+} from '@tanstack/react-query';
 
-interface NoteDetailsPageProps {
-  params: Promise<{
-    id: string;
-  }>;
+import { fetchNoteById } from '@/lib/api';
+import NoteDetailsClient from './NoteDetails.client';
+
+export default async function NoteDetailsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  );
 }
